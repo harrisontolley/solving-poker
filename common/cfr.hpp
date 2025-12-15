182e201c542a09eef2fec55a230524e163b611fb
harrisontolley <htolley0@gmail.com>
2025-12-16 02:44:49 +1100

    Implemented CFR+ and refactored CFR to ABC.

--- common/cfr.hpp
+++ common/cfr.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "commontypes.hpp"
 #include "datawriter.hpp"
 #include <unordered_map>
@@ -8,6 +9,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+
 template <class Game>
 class CFR
 {
@@ -17,33 +19,168 @@ public:
     using InfoSet = typename Game::InfoSet;
 
     explicit CFR(Game game)
-        : game_{std::move(game)} {}
+        : game_{std::move(game)}
+    {
+        // no op
+    }
 
     void train(int num_iterations);
 
-    std::unordered_map<InfoSet, Strategy> get_average_strategy() const;
+    StrategyProfile get_average_strategy() const;
 
     void print_metrics(int num_iterations) const;
 
     void print_strategies() const;
 
-private:
-    Game game_;
-
-    std::unordered_map<InfoSet, Strategy> regret_sum_;
-    std::unordered_map<InfoSet, Strategy> strategy_sum_;
+protected:
+    StrategyProfile regret_sum_;
+    StrategyProfile strategy_sum_;
     std::unordered_map<InfoSet, int> num_actions_;
-
     std::unordered_map<InfoSet, std::vector<Action>> actions_by_infoset_;
 
-    bool write_log_file_ = WRITE_LOG_FILE;
+    virtual void on_regret(InfoSet const &info_set, std::size_t a, double delta) = 0;
+    virtual void on_strategy(InfoSet const &info_set, Strategy const &sigma, double reach) = 0;
 
-    DataWriter data_writer_{LOG_FILE_NAME};
+    int iteration() const noexcept { return iteration_; };
+
+private:
+    // base owned traversal
+    std::pair<double, double> traverse(State const &state, double p1, double p2);
+
+    // ensure vectors are sized and action ordering remembered
+    void ensure_infoset(InfoSet const &info_set, std::vector<Action> const &actions);
 
     Strategy regret_match(InfoSet const &info_set);
 
-    std::pair<double, double> cfr_iterate(State const &state, double p1, double p2);
+private:
+    Game game_;
+
+    int iteration_{0};
+
+    bool write_log_file_ = WRITE_LOG_FILE;
+    DataWriter data_writer_{LOG_FILE_NAME};
 };
+
+template <class Game>
+class CFRVanilla : public CFR<Game>
+{
+public:
+    using CFR<Game>::CFR;
+
+protected:
+    void on_regret(typename Game::InfoSet const &is, std::size_t a, double delta) override
+    {
+        // plain accumulate
+        this->regret_sum_[is][a] += delta;
+    }
+
+    void on_strategy(typename Game::InfoSet const &is, Strategy const &sigma, double reach) override
+    {
+        for (std::size_t a = 0; a < sigma.size(); ++a)
+            this->strategy_sum_[is][a] += reach * sigma[a];
+    }
+};
+
+template <class Game>
+class CFRPlus : public CFR<Game>
+{
+public:
+    using CFR<Game>::CFR;
+
+protected:
+    void on_regret(typename Game::InfoSet const &is, std::size_t a, double delta) override
+    {
+        // CFR+: cumulative regrets are clamped at 0
+        double &r = this->regret_sum_[is][a];
+        r = std::max(0.0, r + delta);
+    }
+
+    void on_strategy(typename Game::InfoSet const &is, Strategy const &sigma, double reach) override
+    {
+        // linear weighting by iteration (t)
+        double w = static_cast<double>(this->iteration());
+        for (std::size_t a = 0; a < sigma.size(); ++a)
+            this->strategy_sum_[is][a] += w * reach * sigma[a];
+    }
+};
+
+template <class Game>
+std::pair<double, double> CFR<Game>::traverse(State const &state, double p1, double p2)
+{
+    if (game_.is_terminal(state))
+        return game_.get_payoffs(state);
+
+    int player = game_.get_current_player(state);
+
+    if (player == CHANCE_PLAYER)
+    {
+        std::pair<double, double> v{0.0, 0.0};
+        for (auto const &[next_state, prob] : game_.enumerate_chance_transitions(state))
+        {
+            auto child = traverse(next_state, p1, p2);
+            v.first += prob * child.first;
+            v.second += prob * child.second;
+        }
+        return v;
+    }
+
+    std::vector<Action> actions = game_.get_legal_actions(state);
+    InfoSet is = game_.get_information_set(state, player);
+
+    ensure_infoset(is, actions);
+
+    Strategy sigma = regret_match(is);
+
+    std::vector<std::pair<double, double>> util(actions.size());
+    std::pair<double, double> node{0.0, 0.0};
+
+    for (std::size_t a = 0; a < actions.size(); ++a)
+    {
+        State next = game_.transition(state, actions[a]);
+
+        util[a] = (player == PLAYER_1)
+                      ? traverse(next, p1 * sigma[a], p2)
+                      : traverse(next, p1, p2 * sigma[a]);
+
+        node.first += sigma[a] * util[a].first;
+        node.second += sigma[a] * util[a].second;
+    }
+
+    // average strategy accumulation for the CURRENT player
+    double reach = (player == PLAYER_1) ? p1 : p2;
+    on_strategy(is, sigma, reach);
+
+    // CFR update (opponent reach weights regrets)
+    if (player == PLAYER_1)
+    {
+        for (std::size_t a = 0; a < actions.size(); ++a)
+            on_regret(is, a, p2 * (util[a].first - node.first));
+    }
+    else
+    {
+        for (std::size_t a = 0; a < actions.size(); ++a)
+            on_regret(is, a, p1 * (util[a].second - node.second));
+    }
+
+    return node;
+}
+
+template <class Game>
+void CFR<Game>::ensure_infoset(InfoSet const &is, std::vector<Action> const &actions)
+{
+    const int n = static_cast<int>(actions.size());
+
+    num_actions_[is] = n;
+    actions_by_infoset_[is] = actions;
+
+    auto &r = regret_sum_[is];
+    if (static_cast<int>(r.size()) != n)
+        r.assign(n, 0.0);
+
+    auto &s = strategy_sum_[is];
+    if (static_cast<int>(s.size()) != n)
+        s.assign(n, 0.0);
+}
 
 template <class Game>
 void CFR<Game>::print_metrics(int num_iterations) const
@@ -96,8 +233,7 @@ Strategy CFR<Game>::regret_match(InfoSet const &info_set)
 }
 
 template <class Game>
-std::unordered_map<typename CFR<Game>::InfoSet, Strategy>
-CFR<Game>::get_average_strategy() const
+StrategyProfile CFR<Game>::get_average_strategy() const
 {
     std::unordered_map<InfoSet, Strategy> average_strategy;
 
@@ -138,8 +274,9 @@ void CFR<Game>::train(int num_iterations)
 
     for (int i = 0; i < num_iterations; ++i)
     {
+        iteration_ = i + 1;
         State s = game_.get_initial_state();
-        cfr_iterate(s, 1.0, 1.0);
+        traverse(s, 1.0, 1.0);
 
         if (write_log_file_ && ((i + 1) % log_every == 0))
         {
@@ -160,76 +297,6 @@ void CFR<Game>::train(int num_iterations)
 
     std::cout << "Training complete.\n";
     print_strategies();
-}
-
-// Single CFR recursion iteration with chance-sampling
-// Returns (counterfactual for p1, counterfactual for p2)
-template <class Game>
-std::pair<double, double>
-CFR<Game>::cfr_iterate(State const &state, double p1, double p2)
-{
-    if (game_.is_terminal(state))
-        return game_.get_payoffs(state);
-
-    int player = game_.get_current_player(state);
-    if (player == CHANCE_PLAYER)
-    {
-        auto [next_state, prob] = game_.chance_transition(state);
-        (void)prob; // unused
-        return cfr_iterate(next_state, p1, p2);
-    }
-
-    InfoSet infoset = game_.get_information_set(state, player);
-    std::vector<Action> actions = game_.get_legal_actions(state);
-    int n = static_cast<int>(actions.size());
-
-    // init storage on first visit
-    if (!regret_sum_.contains(infoset))
-    {
-        regret_sum_[infoset] = Strategy(n, 0.0);
-        strategy_sum_[infoset] = Strategy(n, 0.0);
-        num_actions_[infoset] = n;
-        actions_by_infoset_[infoset] = actions;
-    }
-
-    Strategy sigma = regret_match(infoset);
-
-    double reach = (player == PLAYER_1) ? p1 : p2;
-    for (int i = 0; i < n; ++i)
-        strategy_sum_[infoset][i] += reach * sigma[i];
-
-    // recurse on each action
-    std::vector<std::pair<double, double>> child_values(n);
-    std::pair<double, double> node_value{0.0, 0.0};
-
-    for (int i = 0; i < n; ++i)
-    {
-        State next_state = game_.transition(state, actions[i]);
-        auto child = (player == PLAYER_1)
-                         ? cfr_iterate(next_state, p1 * sigma[i], p2)
-                         : cfr_iterate(next_state, p1, p2 * sigma[i]);
-
-        child_values[i] = child;
-        node_value.first += sigma[i] * child.first;
-        node_value.second += sigma[i] * child.second;
-    }
-
-    double opp_reach = (player == PLAYER_1) ? p2 : p1;
-    int util_idx = (player == PLAYER_1) ? 0 : 1;
-
-    for (int i = 0; i < n; ++i)
-    {
-        double child_util = (util_idx == 0)
-                                ? child_values[i].first
-                                : child_values[i].second;
-        double node_util = (util_idx == 0)
-                               ? node_value.first
-                               : node_value.second;
-        double regret = child_util - node_util;
-        regret_sum_[infoset][i] += opp_reach * regret;
-    }
-
-    return node_value;
 }
 
 template <class Game>
